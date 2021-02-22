90aa74e1e9bbef767dfbbeaf72fded53240abf3a
Anne Cocos <anne@askiggy.com>
2021-02-22 16:47:43 -0500

    add typing to main api module

--- iggyapi/api.py
+++ iggyapi/api.py
@@ -1,9 +1,10 @@
 import requests
 import json
+from typing import Dict
 
 
 class IggyAPI():
-    def __init__(self, api_token):
+    def __init__(self, api_token: str):
         self.api_token = api_token
         self.base_url = "https://api.askiggy.com/v1/"
         self.headers = {
@@ -12,8 +13,8 @@ class IggyAPI():
             "X-Iggy-Token": self.api_token,
         }
 
-    def enrich(self, endpoint: str, options, body={}):
-        method = options["method"] or "GET"
+    def enrich(self, endpoint: str, options: Dict, body: Dict = {}) -> Dict:
+        method = options.get("method") or "GET"
         params = options["params"]
         requestURL = self.base_url + endpoint
 
@@ -26,19 +27,19 @@ class IggyAPI():
                 body), headers=self.headers)
             return r.json()
 
-    def lookup(self, options):
+    def lookup(self, options: Dict) -> Dict:
         return self.enrich("lookup", options)
 
-    def isochrone(self, options):
+    def isochrone(self, options: Dict) -> Dict:
         return self.enrich("isochrone", options)
 
-    def points_of_interest(self, options, body={}):
+    def points_of_interest(self, options: Dict, body: Dict = {}) -> Dict:
         return self.enrich("points_of_interest", options, body)
 
-    def amenities_score(self, options):
+    def amenities_score(self, options: Dict):
         return self.enrich("amenities_score", options)
 
-    def clusters(self, options):
+    def clusters(self, options: Dict):
         return self.enrich("clusters", options)
 
     def points_of_interest_options(self):
